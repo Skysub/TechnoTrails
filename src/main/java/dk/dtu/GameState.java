9626fd4c18594fd31bc92d9baf8a7a6a930147de
skysub <frederik.hedeandersen@gmail.com>
2024-01-08 13:27:18 +0100

    Added a simple gamestate object

---
+++ src/main/java/dk/dtu/GameState.java
@@ -0,0 +1,18 @@
+package dk.dtu;
+
+import java.util.ArrayList;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+public class GameState {
+	int numberOfPlayers = 0;
+	ArrayList<PlayerInfo> players;
+}
+
+class PlayerInfo {
+	int id = -1;
+	float x = 0;
+	float y = 0;
+	float rotation = 0;
+	ArrayList<Pair<Float, Float>> trail;
+}
