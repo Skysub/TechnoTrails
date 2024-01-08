dcafc5e9c625eebb9bad0b93b6895f72e02afd5a
skysub <frederik.hedeandersen@gmail.com>
2024-01-08 14:41:17 +0100

    additional game and gamestate logic

--- src/main/java/dk/dtu/Game.java
+++ src/main/java/dk/dtu/Game.java
@@ -1,20 +1,29 @@
 package dk.dtu;
 
+import java.util.ArrayList;
+
 public class Game {
+	
 	
 	Game(){
 	}
 	
 	//The game simulates a tick and handles all the game mechanics
 	GameState Step(GameState oldState) {
+		GameState newState = new GameState();
+		
+		if(!oldState.paused) {
+			
+		}
 		
 		return null;
 	}
 	
 	//Creates a fresh game
-	GameState CreateGameState(int players) {
+	GameState CreateGameState(ArrayList<PlayerInfo> players) {
 		GameState freshState = new GameState();
-		freshState.players.add(new PlayerInfo());
+		freshState.players = players;
+		freshState.numberOfPlayers = players.size();
 		return freshState;
 	}
 }
--- src/main/java/dk/dtu/GameState.java
+++ src/main/java/dk/dtu/GameState.java
@@ -7,10 +7,14 @@ import org.apache.commons.lang3.tuple.Pair;
 public class GameState {
 	int numberOfPlayers = 0;
 	ArrayList<PlayerInfo> players;
+	
+	int gameTime; //In ms
+	boolean paused = true;
 }
 
 class PlayerInfo {
 	int id = -1;
+	String name = "noName";
 	float x = 0;
 	float y = 0;
 	float rotation = 0;
