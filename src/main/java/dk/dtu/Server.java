8f55bc5d2506d67d9f639006f9864cabbde0267c
Martin Jespersen <martinjespersen9@gmail.com>
2024-01-11 09:17:19 +0100

    refactor server/client

--- src/main/java/dk/dtu/Server.java
+++ src/main/java/dk/dtu/Server.java
@@ -1,24 +1,47 @@
 package dk.dtu;
 
-public class Server{
-	
-	ServerInfo info;
-	
-	Server(){
-		info = new ServerInfo();
-	}
-	
-	//Functions dealing with the server side of networking 
-	
-	
-	
-	//Closes the server and prepares this object for deletion
-	void kill() {
-		
-	}
-	
-	ServerInfo getInfo() {
-		return info;
-	}
-	
+import java.util.ArrayList;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.jspace.SequentialSpace;
+import org.jspace.Space;
+import org.jspace.SpaceRepository;
+
+public class Server {
+
+    ServerInfo info;
+    Space lobbySpace;
+    Space chatSpace;
+    static final int defaultTickRate = 60;
+    String hostAddress = "localhost";
+
+    Server() {
+        info = new ServerInfo();
+        info.tps = defaultTickRate;
+        info.playerList = new ArrayList<ImmutablePair<Integer, String>>();
+    }
+
+    public void createLobby(String hostName) {
+        info.addPlayer(hostName);
+        SpaceRepository repository = new SpaceRepository();
+        repository.addGate("tcp://" + hostAddress + ":9001/?keep");
+        lobbySpace = new SequentialSpace();
+        repository.add("lobby", lobbySpace);
+        chatSpace = new SequentialSpace();
+        repository.add("chat", chatSpace);
+
+        try {
+            lobbySpace.put(hostName);
+            System.out.println("You have created and joined the lobby");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void kill() {
+        // Add server shutdown logic here
+    }
+
+    ServerInfo getInfo() {
+        return info;
+    }
 }
